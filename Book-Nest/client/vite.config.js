e2868ec0b0b8144e2576d9fae5a542fb7cd445bd
Kowshik Valipireddy <139107793+kowshik3383@users.noreply.github.com>
2024-03-17 01:09:08 +0530

    Update vite.config.js

--- Book-Nest/client/vite.config.js
+++ Book-Nest/client/vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':{
-        target:'https://mern-book-nest.vercel.app/',
-        secure:false
-      },
-    },
-  },
-  plugins: [react()],
-})
+  plugins: [react()]
+});
