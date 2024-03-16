423c0ecf334147501d004e38acb100165c3aa404
Kowshik Valipireddy <139107793+kowshik3383@users.noreply.github.com>
2024-03-17 01:06:54 +0530

    Update vite.config.js

--- Book-Nest/client/vite.config.js
+++ Book-Nest/client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:'http://localhost:5000',
+        target:'https://mern-book-nest.vercel.app/',
         secure:false
       },
     },
