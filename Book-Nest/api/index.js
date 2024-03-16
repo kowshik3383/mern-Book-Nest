1816f338c130b75a07ac6cd62a5a59cc2a8fa27d
Kowshik Valipireddy <139107793+kowshik3383@users.noreply.github.com>
2024-03-17 00:51:41 +0530

    Update index.js

--- Book-Nest/api/index.js
+++ Book-Nest/api/index.js
@@ -1,24 +1,29 @@
+// app.mjs
+
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
 import authRoute from './routes/auth.route.js';
-import userRoute from './routes/user.route.js'; 
+import userRoute from './routes/user.route.js';
+
 const app = express();
 const uri = 'mongodb://localhost:27017/mydata';
 const port = 5000;
-import cors from 'cors';
 
 // Connect to MongoDB
 mongoose.connect(uri)
   .then(() => console.log('Connected to MongoDB successfully'))
   .catch((err) => console.error('Error connecting to MongoDB:', err));
 
+// Set CORS headers
+app.use(cors());
+
 // Middleware to parse JSON bodies
 app.use(express.json());
-app.use(cors());
 
 // Route for handling requests related to users
 app.use('/api/user', userRoute);
-app.use('/api/auth',authRoute);
+app.use('/api/auth', authRoute);
 
 // Error handler for invalid routes
 app.use((req, res, next) => {
@@ -28,16 +33,13 @@ app.use((req, res, next) => {
 });
 
 // Global error handler
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   res.status(err.status || 500).json({
     error: {
       message: err.message
     }
   });
 });
-
-
-
 
 // Start the server
 app.listen(port, () => {
